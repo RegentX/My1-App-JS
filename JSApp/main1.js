1b5a963c742a3e80856aa46fd19ac0344cb64f84
ALex <S4519040@outlook.com>
2022-06-19 22:42:31 +0300

    Practice 2

--- JSApp/main1.js
+++ JSApp/main1.js
@@ -2,6 +2,7 @@
 "use strict";
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?: ");
 let arr = [];
+let i;
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,12 +10,34 @@ const personalMovieDB = {
     genres: arr,
     privat: false
 };
+for (i=0;i<2;i++){
+    let namef = prompt("Один из последних просмотренных фильмов?", ""),
+        assess = prompt("На сколько оцените его?", "");
+    
+    if (namef != null && assess != null && namef.length < 50 && assess.length < 50 && namef != '' && assess != ''){
+        personalMovieDB.movies[namef] = assess;
+        console.log("alright");
+    }else{
+        i--;
+        console.log("error");
+    }
+}
 
-let namef = prompt("Один из последних просмотренных фильмов?", ""),
+if (personalMovieDB.count < 10){
+    alert("There are too much u should watch! Keep watchin!");
+}else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+    alert("You are a classic viewer!");
+}else if(personalMovieDB.count > 30){
+    alert("You are a movie buff");
+}else{
+    console.log("ERROR");
+    alert("ERROR");
+}
+/*let namef = prompt("Один из последних просмотренных фильмов?", ""),
     assess = prompt("На сколько оцените его?", ""),
     namef2 = prompt("Один из последних просмотренных фильмов?", ""),
     assess2 = prompt("На сколько оцените его?", "");
 personalMovieDB.movies[namef] = assess;
 personalMovieDB.movies[namef2] = assess2;
 
-console.log(personalMovieDB);
+console.log(personalMovieDB);*/
